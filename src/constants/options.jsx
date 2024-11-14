1e784ccb9177bf65abcc5936c40fced58fe76bb6
akumi07 <akash9338144396@gmail.com>
2024-11-14 12:11:47 +0530

    Day 2

---
+++ src/constants/options.jsx
@@ -0,0 +1,59 @@
+export const SelectTravelesList=[
+    {
+        id: 1,
+        title:'Just Me',
+        desc:'A sole traveles in exploration',
+        icon:'✈️',
+        person:'1'
+    },
+    {
+        id: 2,
+        title:'Couple',
+        desc:'Two traveles in tandem',
+        icon:'🥂',
+        person:'2'
+    },
+    {
+        id: 3,
+        title:'Family',
+        desc:'A group of fun loving adv',
+        icon:'🏡',
+        person:'3 to 5 person'
+    },
+    {
+        id: 4,
+        title:'Friends',
+        desc:'A bunch of thrill-seekes',
+        icon:'⛵',
+        person:'5 to 10 people'
+    }
+
+    
+]
+
+export const selectBudgetOptions=[
+    {
+        id: 1,
+        title:'Cheap',
+        desc:'Stay Conscious of Cost',
+        icon:'💵',
+        
+    },
+    {
+        id: 2,
+        title:'Moderate',
+        desc:'Keep cost on the average side',
+        icon:'💰',
+        
+    },
+    {
+        id: 3,
+        title:'Luxury',
+        desc:'Dont worry about cost',
+        icon:'💸',
+        
+    },
+
+]
+
+export const AI_PROMPT='Generate travel Plan for Location: {location}'
