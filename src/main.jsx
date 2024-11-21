8406022115c222dc894f7e99d7c74714e3a6dd03
akumi07 <akash9338144396@gmail.com>
2024-11-21 13:46:55 +0530

    Revert "Revert "Day 4""
    This reverts commit 706f0be4174c688d468d831ee7f50e820ad3ec32.

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import CreateTrip from './create-trip'
 import Header from './components/custom/Header'
 import { Toaster } from './components/ui/sonner'
 import Viewtrip from './view-trip/[tripid]'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 
 
@@ -28,8 +30,10 @@ const router=createBrowserRouter([
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
     <Header/>
     <Toaster/>
     <RouterProvider router={router}/>
+    </GoogleOAuthProvider>;
   </StrictMode>,
 )
